cceaa227403ac2588fb8a5b78fd7fd694fec756a
MisoYuri <oraclemiso@naver.com>
2023-05-24 05:48:24 +0000

    Add Final Version

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue May  9 06:42:42 2023
-__version__ = '1.2.0+0d011c0'
+# TIME: Mon May 22 06:25:09 2023
+__version__ = '1.2.0+d0238f0'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
